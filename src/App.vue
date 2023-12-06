c1d447eda092f20a1b8cc77c0d0b52a3ca362303
Kevin <kevin.paez1010@gmail.com>
2023-12-06 10:35:34 -0600

    [feat] - templated setup

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Template from './components/Template.vue';
+</script>
+
+<template>
+  <Template msg="This is a template" />
+</template>
+
+<style scoped></style>
---
+++ src/components/Template.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+defineProps({
+  msg: {
+    type: String,
+  }
+})
+</script>
+
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+
+<style scoped>
+h1 {
+  color: #fff;
+}
+</style>
